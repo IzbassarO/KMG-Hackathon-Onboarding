0c9715c3466e2795ce288dc6208275dbc1dbc604
Izbassar <izok2004@gmail.com>
2026-06-05 22:36:08 +0500

    Initial Commit

---
+++ Onboard/OnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardApp.swift
+//  Onboard
+//
+//  Created by Izbassar Orynbassar on 05.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct OnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
